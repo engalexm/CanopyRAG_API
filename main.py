2567b378772a2d41bfd5b702fe472f8051a2adf2
Alex Eng <engalexm@gmail.com>
2024-04-11 12:11:23 -0400

    Try init outside of Lambda function

--- main.py
+++ main.py
@@ -10,25 +10,24 @@ from canopy.context_engine import ContextEngine
 from canopy.chat_engine import ChatEngine
 from canopy.models.data_models import UserMessage
 
+# Initialize Canopy
+Tokenizer.initialize()
+kb = KnowledgeBase(index_name="canopy--citi-wealth-outlook-2024")
+kb.connect()
+context_engine = ContextEngine(kb)
+chat_engine = ChatEngine(context_engine)
 
 # Query Canopy and Pinecone
 def ask_canopy_rag(event, context):
     try:
-        # Initialize Canopy
-        Tokenizer.initialize()
-        kb = KnowledgeBase(index_name="canopy--citi-wealth-outlook-2024")
-        kb.connect()
-        context_engine = ContextEngine(kb)
-        chat_engine = ChatEngine(context_engine)
-
         res = ""
-        request_body = event
 
-        if 'query' in request_body:
-            res = chat_engine.chat(messages=[UserMessage(content=request_body["query"])], stream=False)      
+        if 'query' in event:
+            res = chat_engine.chat(messages=[UserMessage(content=event["query"])], stream=False)      
             res = res.choices[0].message.content
+
         return res
 
     except Exception as exc:
         print(traceback.format_exc())
-        return ""
+        return traceback.format_exc()
